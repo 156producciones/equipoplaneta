4dc61cbe768635de8aa0085f488426a1409d779a
156producciones <informatica@156producciones.com>
2023-08-08 10:14:09 +0200

    Navigator view complete. Element adapted to all screen sizes

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+
+const burguerBtn = document.getElementById('burguer')
+
+burguerBtn.addEventListener('click', () => {
+    document.getElementById('menu-container').style.display = 'flex'
+    document.getElementById('menu-container').style.animation = 'fadeIn 0.5s'
+    document.getElementById('menu').style.animation = 'leftMovement 0.5s'
+})
+
+document.getElementById('menu-actions-close').addEventListener('click', () => {
+    document.getElementById('menu').style.animation = 'rightMovement 0.5s'
+    document.getElementById('menu-container').style.animation = 'fadeOut 0.5s'
+    setTimeout(() => {
+        document.getElementById('menu-container').style.display = 'none'
+    }, 400);
+})
